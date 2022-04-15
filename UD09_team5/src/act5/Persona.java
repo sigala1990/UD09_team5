113a8262884b9ddbff6820f7b54f5ed0c275d705
IsidreP <103040138+IsidreP@users.noreply.github.com>
2022-04-15 12:55:49 +0200

    metodos finalizados

--- UD09_team5/src/act5/Persona.java
+++ UD09_team5/src/act5/Persona.java
@@ -1,27 +1,74 @@
 package act5;
 
+import act5.Persona.Genero;
+
 public abstract class Persona {
 
-	
+	public enum Genero{hombre, mujer};
 	protected String nombre;
 	protected int edad;
-	protected String sexo;
+	protected Genero sexo;
 	
 	
 	
 	public Persona() {
-		super();
-		this.nombre = "";
-		this.edad = 0;
-		this.sexo = "";
+		//super();
+		this.nombre = sacarNombre(arrayMujerNombre(), arrayHombreNombre());
+		this.edad = edadAleatorio ();
+		this.sexo = sexoAleatorio(arrayMujerNombre(),arrayHombreNombre());
 	}
 	
-	public Persona(String nombre, int edad, String sexo) {
-		super();
+	public Persona(String nombre, int edad/*, Object sexo*/) {
+		//super();
 		this.nombre = nombre;
 		this.edad = edad;
-		this.sexo = sexo;
+		//this.sexo =sexo ;
 	}
+	
+	//metodo si hombre o mujer en funcion del nombre
+	public Genero sexoAleatorio (String[] nombreMujer, String[] nombreHombre) {
+
+		for (int i = 0; i < nombreHombre.length; i++) {
+			if(nombre == nombreMujer[i]) {
+				return Genero.mujer;
+			}
+			if(nombre == nombreHombre[i]) {
+				return Genero.hombre;
+			}
+		}
+		return null;	
+	}
+	
+	public String[] arrayHombreNombre() {
+		String[] nomHombre = {"Toni", "Ivan", "Marc", "Pedro"};
+		return nomHombre;
+	
+		
+	}
+	public String[] arrayMujerNombre() {
+		String[] nomMujer = {"Eva", "Ana", "Sonia", "Marta"};
+		return nomMujer ;
+	}
+	
+	// Metodo para sacar nombre
+	public String sacarNombre (String[] nombreMujer, String[] nombreHombre){
+				
+		int x= (int) Math.random()*(4-0)+0;
+
+		int y= (int) (Math.random() * (4 - 1) + 1);
+		if(x >2) {
+			return nombreHombre[y];
+			
+		}else{
+				return nombreMujer[y];
+		}
+	}
+	
+	//Metodo abstracto para implementar en profesor y estudiante
+	public abstract boolean ausenciaAleatorio ();
+
+	//Metodo abstracto para implementar en profesor y estudiante
+	public abstract int edadAleatorio ();
 	
 	
 	public String getNombre() {
@@ -44,17 +91,17 @@ public abstract class Persona {
 	}
 
 
-	public String getSexo() {
+	public Genero getSexo() {
 		return sexo;
 	}
 
 
-	public void setSexo(String sexo) {
+	public void setSexo(Genero sexo) {
 		this.sexo = sexo;
 	}
 
 
-	//public abstract boolean plus (double sueldoPlus);
+
 	
 	
 }
